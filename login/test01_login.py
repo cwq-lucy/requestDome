ff9b3fe1d263acfe7a1379dd56a0524e7c2c339e
chenwenqi <chenwenqi@qq.com>
2022-04-26 16:31:13 +0800

    push

--- login/test01_login.py
+++ login/test01_login.py
@@ -1,4 +1,5 @@
 import unittest
+import requests
 from  ddt import ddt,data,unpack,file_data
 
 @ddt
@@ -6,10 +7,12 @@ class loginCass(unittest.TestCase):
      # @data(('zhangsan','123456'),('lisi','123456'))
      # @unpack
      @file_data('login.yaml')
-     def test_login(self,username,password):
-          data = {'username':username,'password':password}
-          print(data)
-
+     def test_login(self,loginName,encryption,yzm,userType,msgCode):
+          data = {"loginName":loginName,"encryption":encryption,"yzm":yzm,"userType":userType,"msgCode":msgCode}
+          url = 'https://cms-api-test.jiaoyoushow.com/userInfo/checkPhoneLoginPost'
+          res = requests.post(url,json=data)
+          msg = res.json()
+          print(msg)
 
 
 if __name__ == '__main__':
---
+++ login/test02_login.py
@@ -0,0 +1,16 @@
+import unittest
+import requests
+from  ddt import ddt,data,unpack,file_data
+
+@ddt
+class loginCass(unittest.TestCase):
+     # @data(('zhangsan','123456'),('lisi','123456'))
+     # @unpack
+     @file_data('login01.yaml')
+     def test_login(self,username,password):
+          data = {"username":username,"password":password}
+          print(data)
+
+
+if __name__ == '__main__':
+     unittest.main()
